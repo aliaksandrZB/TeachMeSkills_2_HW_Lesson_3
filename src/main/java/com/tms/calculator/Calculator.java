db1d57e8cbb62dea7069c509b479b560870d20c4
aliaksandrZB <s.demeshik.s@gmail.com>
2022-03-09 16:49:51 +0100

    created a calculator web application

---
+++ src/main/java/com/tms/calculator/Calculator.java
@@ -0,0 +1,47 @@
+package com.tms.calculator;
+
+import com.tms.servlet.ServletCalculator;
+
+public class Calculator {
+    
+    private static double addition(double num1, double num2) {
+        return num1 + num2;
+    }
+     
+    private static double subtraction(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    private static double multiplication(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    private static double division(double num1, double num2) {
+        if (num2 == 0) {
+            ServletCalculator.bugReport = "Division by zero is not allowed";
+            return 0;
+        }
+        return num1 / num2;
+    }
+
+    public static double performOperation(double num1, double num2, String operation) {
+        switch (operation) {
+            case "add":
+                ServletCalculator.sign = "+";
+                return addition(num1, num2);
+            case "sub":
+                ServletCalculator.sign = "-";
+                return subtraction(num1, num2);
+            case "mult":
+                ServletCalculator.sign = "*";
+                return multiplication(num1, num2);
+            case "div":
+                ServletCalculator.sign = "/";
+                return division(num1, num2);
+            default:
+                ServletCalculator.bugReport = "Incorrect operation";
+                return 0;
+        }
+    }
+    
+}
---
+++ src/main/java/com/tms/servlet/ServletCalculator.java
@@ -0,0 +1,38 @@
+package com.tms.servlet;
+
+import com.tms.calculator.Calculator;
+
+import javax.jws.WebService;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/calc")
+public class ServletCalculator extends HttpServlet {
+
+    public static String bugReport = null;
+    public static String sign = null;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String num1 = req.getParameter("num1");
+        String num2 = req.getParameter("num2");
+        String operation = req.getParameter("operation");
+
+        double sum = Calculator.performOperation(Double.parseDouble(num1), Double.parseDouble(num2), operation);
+
+        if (bugReport == null) {
+            resp.getWriter().println(Double.parseDouble(num1) + " " + sign + " " +
+                                     Double.parseDouble(num2) + " = " +
+                                     sum);
+            sign = null;
+        } else {
+            resp.getWriter().println(bugReport);
+            bugReport = null;
+        }
+    }
+
+}
